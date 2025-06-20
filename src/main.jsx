c5a8eb934456afa33d8428126ddabeae6fb68a2b
istoleurk1ll <bigmofo007@gmail.com>
2025-06-20 16:54:17 -0500

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import FairwayPhantom from "./FairwayPhantom.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <FairwayPhantom />
+  </React.StrictMode>
+);

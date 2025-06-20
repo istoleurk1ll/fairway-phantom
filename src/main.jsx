dce067795272a72edc5d6fdae6040a38ff2c14d8
istoleurk1ll <bigmofo007@gmail.com>
2025-06-20 16:52:28 -0500

    Delete src directory

--- src/main.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import FairwayPhantom from "./FairwayPhantom.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(<FairwayPhantom />);
